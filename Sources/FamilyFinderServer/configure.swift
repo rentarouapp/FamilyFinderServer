9729d840ce08bad2313a6406677a254f9d4a4617
uejo <rentarou1994@gmail.com>
2025-05-01 16:44:16 +0900

    :art: いったんTLSはconfigure.swift側で無効にする

--- Sources/FamilyFinderServer/configure.swift
+++ Sources/FamilyFinderServer/configure.swift
@@ -14,7 +14,7 @@ public func configure(_ app: Application) async throws {
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
+        tls: .disable)
     ), as: .psql)
 
     app.migrations.add(CreateTodo())
